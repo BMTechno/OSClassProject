5829c7224aa056fc949821711c5c057133895c1e
BppleMan <1286363484@qq.com>
2017-04-06 22:01:37 +0800

    黄文和的第二次修改

--- src/com/bppleman/BppleMan.java
+++ src/com/bppleman/BppleMan.java
@@ -5,5 +5,8 @@ public class BppleMan
 	public static void main(String[] args)
 	{
 		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
 	}
 }
