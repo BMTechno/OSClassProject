eeba16feceba3d3e41ee817eb4374c5c9cd9de32
BppleMan <1286363484@qq.com>
2017-04-18 20:46:35 +0800

    push测试

--- src/com/zzl/FreeNode.java
+++ src/com/zzl/FreeNode.java
@@ -1,34 +1,42 @@
 package com.zzl;
 
-public class FreeNode {
+public class FreeNode
+{
 	private long begin;
 	private long size;
 
-	public FreeNode(long begin, long size) {
+	public FreeNode(long begin, long size)
+	{
 		super();
 		this.begin = begin;
 		this.size = size;
 	}
 
-	public long getBegin() {
+	public long getBegin()
+	{
 		return begin;
 	}
 
-	public void setBegin(long begin) {
+	public void setBegin(long begin)
+	{
 		this.begin = begin;
 	}
 
-	public long getSize() {
+	public long getSize()
+	{
 		return size;
 	}
 
-	public void setSize(long size) {
+	public void setSize(long size)
+	{
 		this.size = size;
 	}
 
 	@Override
-	public String toString() {
+	public String toString()
+	{
 		return "FreeNode [begin=" + begin + ", size=" + size + "]";
 	}
 
+	// push测试
 }
